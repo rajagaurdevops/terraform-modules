7993c32318ca1fc7d35ba19c458cbd621f4ae0c0
rajagaurdevops <rajagaur333@gmail.com>
2025-10-28 14:42:55 +0530

    Create variables.tf

---
+++ vic-module/variables.tf
@@ -0,0 +1,30 @@
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability Zone for subnets"
+  type        = string
+}
+
+variable "tags" {
+  description = "Additional tags for all resources"
+  type        = map(string)
+  default     = {}
+}

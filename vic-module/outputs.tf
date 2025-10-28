b14ad4f5d9c0490be213d9e3e3d15ba10022016f
rajagaurdevops <rajagaur333@gmail.com>
2025-10-28 14:43:40 +0530

    Create outputs.tf

---
+++ vic-module/outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private_subnet.id
+}
+
+output "igw_id" {
+  description = "The Internet Gateway ID"
+  value       = aws_internet_gateway.this.id
+}
+
+output "public_route_table_id" {
+  description = "The ID of the public route table"
+  value       = aws_route_table.public_rt.id
+}
